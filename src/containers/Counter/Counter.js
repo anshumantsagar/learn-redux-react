c7f41f5a392b4e1992bb7f010cf26daa31a8a5a6
anshumantsagar <anshumantsagar@gmail.com>
2020-06-25 04:49:19 +0530

    redux implimented

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,42 +1,54 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
-
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-            default:    
-        }
-    }
-
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 10" clicked={this.props.onSubCounter}  />
+                <hr/>
+                <button onClick={this.props.storeResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResult.map(li => (
+                        <li key={li.id} onClick={() => this.props.onDeleteResult(li.id)}>{li.value}</li>))}
+                </ul>
             </div>
         );
     }
 }
 
-export default Counter;
+//mapStateToProps stores a function which expects state stored in redux  as input and return a javascript object which is a map of prop names
+//and slices of the state stored in redux
+const mapStateToProps = state => {
+    return {
+        ctr: state.counter,
+        storedResult: state.results
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
+        onAddCounter: () => dispatch({type: 'ADD', val:10}),
+        onSubCounter: () => dispatch({type: 'SUB', val:10}),
+        storeResult: () => dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult: (id) => dispatch({type: 'DELETE_RESULT', resultElId:id})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+//import { connect } from 'react-redux';
+//then wrap the function 'connect' from the react-redux which return the hoc
+// then map the state to the props as above with const mapStateToProps
+//then we pass this to connect 
+//connect then gives us gives us the Coutnter.js state along with the ctr in the mapStateToProps function
+// now define a function where we can dispatch our methodes to the reducers like in mapDispatchToProps
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './store/reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
+
+// import { createStore } from redux.
+// create a const name store = createStore.
+// create a reducer.js file like in this project and then import reducer and pass the reducer here created like in reducer.js. 
+// npm install react-redux (it helps us hook up our redux store to our react application).
+// import { Provide } from 'react-redux' and wrap the <App/> with it like above.
+// and for hooking up the provider component with our store we need to set up a special property called store expected bu the component.
+// accessing data from the store we will do it in the counter.js
+
